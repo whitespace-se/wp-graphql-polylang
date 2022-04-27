40b3991d701e993fad864331e9e201b4b2d119f6
Fredrik Johansson <jeanfredrik@gmail.com>
2022-04-27 16:14:45 +0200

    feat: Add NodeWithLanguage interface

--- src/PostObject.php
+++ src/PostObject.php
@@ -12,7 +12,7 @@ class PostObject
             'graphql_register_types',
             [$this, '__action_graphql_register_types'],
             10,
-            0
+            1
         );
 
         add_action(
@@ -62,7 +62,7 @@ class PostObject
         }
     }
 
-    function __action_graphql_register_types()
+    function __action_graphql_register_types($type_registry)
     {
         register_graphql_fields('RootQueryToContentNodeConnectionWhereArgs', [
             'language' => [
@@ -80,6 +80,169 @@ class PostObject
                     'Filter content nodes by one or more languages (Polylang)',
             ],
         ]);
+
+        register_graphql_interface_type(
+            'NodeWithLanguage',
+            [
+                'description' =>
+                    'A node that has a language code (Polylang)',
+                'resolveType' => function ( \WPGraphQL\Model\Post $post ) use ( $type_registry ) {
+                    return $type_registry->get_type('ContentNode')->config['resolveType']( $post );
+                },
+                'fields' => [
+                    'language' => [
+                        'type' => 'Language',
+                        'description' => __('Polylang language', 'wpnext'),
+                        'resolve' => function (
+                            \WPGraphQL\Model\Post $post,
+                            $args,
+                            $context,
+                            $info
+                        ) {
+                            $fields = $info->getFieldSelection();
+                            $language = [
+                                'name' => null,
+                                'slug' => null,
+                                'code' => null,
+                            ];
+
+                            $post_id = $post->ID;
+
+                            // The language of the preview post is not set at all so we
+                            // must get the language using the original post id
+                            if ($post->isPreview) {
+                                $post_id = wp_get_post_parent_id($post->ID);
+                            }
+
+                            $slug = pll_get_post_language($post_id, 'slug');
+
+                            if (!$slug) {
+                                return null;
+                            }
+
+                            $language['code'] = $slug;
+                            $language['slug'] = $slug;
+                            $language['id'] = Relay::toGlobalId('Language', $slug);
+
+                            if (isset($fields['name'])) {
+                                $language['name'] = pll_get_post_language(
+                                    $post_id,
+                                    'name'
+                                );
+                            }
+
+                            if (isset($fields['locale'])) {
+                                $language['locale'] = pll_get_post_language(
+                                    $post_id,
+                                    'locale'
+                                );
+                            }
+
+                            return $language;
+                        },
+                    ],
+                    'translation' => [
+                        'type' => 'NodeWithLanguage',
+                        // 'resolveType' => function (
+                        //     \WPGraphQL\Model\Post $post,
+                        //     $args,
+                        //     $context,
+                        //     $info
+                        // ) {
+                        //     return $info->parentType;
+                        // },
+                        'description' => __(
+                            'Get specific translation version of this object',
+                            'wp-graphql-polylang'
+                        ),
+                        'args' => [
+                            'language' => [
+                                'type' => [
+                                    'non_null' => 'LanguageCodeEnum',
+                                ],
+                            ],
+                        ],
+                        'resolve' => function (
+                            \WPGraphQL\Model\Post $post,
+                            array $args
+                        ) {
+                            $translations = pll_get_post_translations($post->ID);
+                            $post_id = $translations[$args['language']] ?? null;
+        
+                            if (!$post_id) {
+                                return null;
+                            }
+        
+                            return new \WPGraphQL\Model\Post(
+                                \WP_Post::get_instance($post_id)
+                            );
+                        },
+                    ],
+                    'translations' => [
+                        'type' => ['list_of' => 'NodeWithLanguage'],
+                        // 'resolveType' => function (
+                        //     \WPGraphQL\Model\Post $post,
+                        //     $args,
+                        //     $context,
+                        //     $info
+                        // ) {
+                        //     return ['list_of' => $info->parentType];
+                        // },
+                        'description' => __(
+                            'List all translated versions of this post',
+                            'wp-graphql-polylang'
+                        ),
+                        'resolve' => function (\WPGraphQL\Model\Post $post) {
+                            $posts = [];
+
+                            if ($post->isPreview) {
+                                $parent = wp_get_post_parent_id($post->ID);
+                                $translations = pll_get_post_translations($parent);
+                            } else {
+                                $translations = pll_get_post_translations($post->ID);
+                            }
+
+                            foreach ($translations as $lang => $post_id) {
+                                $translation = \WP_Post::get_instance($post_id);
+
+                                if (!$translation) {
+                                    continue;
+                                }
+
+                                if (is_wp_error($translation)) {
+                                    continue;
+                                }
+
+                                if ($post->ID === $translation->ID) {
+                                    continue;
+                                }
+
+                                // If fetching preview do not add the original as a translation
+                                if ($post->isPreview && $parent === $translation->ID) {
+                                    continue;
+                                }
+
+                                $model = new \WPGraphQL\Model\Post($translation);
+
+                                // If we do not filter out privates here wp-graphql will
+                                // crash with 'Cannot return null for non-nullable field
+                                // Post.id.'. This might be a wp-graphql bug.
+                                // Interestingly only fetching the id of the translated
+                                // post caused the crash. For example title is ok even
+                                // without this check
+                                if ($model->is_private()) {
+                                    continue;
+                                }
+
+                                $posts[] = $model;
+                            }
+
+                            return $posts;
+                        },
+                    ]
+                ],
+            ]
+        );
 
         foreach (\WPGraphQL::get_allowed_post_types() as $post_type) {
             $this->add_post_type_fields(get_post_type_object($post_type));
@@ -121,155 +284,9 @@ class PostObject
             ],
         ]);
 
-        register_graphql_field(
-            $post_type_object->graphql_single_name,
-            'language',
-            [
-                'type' => 'Language',
-                'description' => __('Polylang language', 'wpnext'),
-                'resolve' => function (
-                    \WPGraphQL\Model\Post $post,
-                    $args,
-                    $context,
-                    $info
-                ) {
-                    $fields = $info->getFieldSelection();
-                    $language = [
-                        'name' => null,
-                        'slug' => null,
-                        'code' => null,
-                    ];
-
-                    $post_id = $post->ID;
-
-                    // The language of the preview post is not set at all so we
-                    // must get the language using the original post id
-                    if ($post->isPreview) {
-                        $post_id = wp_get_post_parent_id($post->ID);
-                    }
-
-                    $slug = pll_get_post_language($post_id, 'slug');
-
-                    if (!$slug) {
-                        return null;
-                    }
-
-                    $language['code'] = $slug;
-                    $language['slug'] = $slug;
-                    $language['id'] = Relay::toGlobalId('Language', $slug);
-
-                    if (isset($fields['name'])) {
-                        $language['name'] = pll_get_post_language(
-                            $post_id,
-                            'name'
-                        );
-                    }
-
-                    if (isset($fields['locale'])) {
-                        $language['locale'] = pll_get_post_language(
-                            $post_id,
-                            'locale'
-                        );
-                    }
-
-                    return $language;
-                },
-            ]
-        );
-
-        register_graphql_field(
-            $post_type_object->graphql_single_name,
-            'translation',
-            [
-                'type' => $type,
-                'description' => __(
-                    'Get specific translation version of this object',
-                    'wp-graphql-polylang'
-                ),
-                'args' => [
-                    'language' => [
-                        'type' => [
-                            'non_null' => 'LanguageCodeEnum',
-                        ],
-                    ],
-                ],
-                'resolve' => function (
-                    \WPGraphQL\Model\Post $post,
-                    array $args
-                ) {
-                    $translations = pll_get_post_translations($post->ID);
-                    $post_id = $translations[$args['language']] ?? null;
-
-                    if (!$post_id) {
-                        return null;
-                    }
-
-                    return new \WPGraphQL\Model\Post(
-                        \WP_Post::get_instance($post_id)
-                    );
-                },
-            ]
-        );
-
-        register_graphql_field(
-            $post_type_object->graphql_single_name,
-            'translations',
-            [
-                'type' => [
-                    'list_of' => $type,
-                ],
-                'description' => __(
-                    'List all translated versions of this post',
-                    'wp-graphql-polylang'
-                ),
-                'resolve' => function (\WPGraphQL\Model\Post $post) {
-                    $posts = [];
-
-                    if ($post->isPreview) {
-                        $parent = wp_get_post_parent_id($post->ID);
-                        $translations = pll_get_post_translations($parent);
-                    } else {
-                        $translations = pll_get_post_translations($post->ID);
-                    }
-
-                    foreach ($translations as $lang => $post_id) {
-                        $translation = \WP_Post::get_instance($post_id);
-
-                        if (!$translation) {
-                            continue;
-                        }
-
-                        if (is_wp_error($translation)) {
-                            continue;
-                        }
-
-                        if ($post->ID === $translation->ID) {
-                            continue;
-                        }
-
-                        // If fetching preview do not add the original as a translation
-                        if ($post->isPreview && $parent === $translation->ID) {
-                            continue;
-                        }
-
-                        $model = new \WPGraphQL\Model\Post($translation);
-
-                        // If we do not filter out privates here wp-graphql will
-                        // crash with 'Cannot return null for non-nullable field
-                        // Post.id.'. This might be a wp-graphql bug.
-                        // Interestingly only fetching the id of the translated
-                        // post caused the crash. For example title is ok even
-                        // without this check
-                        if ($model->is_private()) {
-                            continue;
-                        }
-
-                        $posts[] = $model;
-                    }
-
-                    return $posts;
-                },
-            ]
+        register_graphql_interfaces_to_types(
+            ["NodeWithLanguage"],
+            [$type]
         );
     }
 
